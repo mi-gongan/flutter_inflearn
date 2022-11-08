47392da2dce3feba30fc93678cf6756848e3d4c6
INBUM02 <kib0202@naver.com>
2022-11-09 00:14:39 +0900

    FEAT: couple date

---
+++ lib/couple_date.dart
@@ -0,0 +1,35 @@
+import "package:flutter/material.dart";
+import 'package:inflearn/screen/date_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontFamily: 'sunflower',
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.white,
+            fontFamily: 'sunflower',
+            fontSize: 30.0,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.white,
+            fontFamily: 'sunflower',
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+      home: DateScreen(),
+    ),
+  );
+}
